54a8979f64d298e892355bcdda74362291737958
Sumit Mishra <sumitmishrayt007@gmail.com>
2025-12-05 19:00:07 +0530

    Add files via upload

---
+++ if_else_statement.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    //WAP that takes an input of age 
+    // and prints if you are adult or not 
+
+    int age;
+    cin >> age;
+    if(age >= 18)
+    {
+        cout<<"You are an adult ";
+    }
+    else{
+        cout<<"You are not adult ";
+    }
+    return 0;
+}
