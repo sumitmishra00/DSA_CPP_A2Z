8eee65f5fb5aa21d05fdc1254f7f9f489e5c0cd2
Sumit Mishra <sumitmishrayt007@gmail.com>
2025-12-06 15:47:11 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 1 D array 
+int main(){
+ /*   int arr[5];
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    cout<<arr[3]; */
+
+    // 2D array example
+    
+    int arr[2][4];
+    arr[2][4] = 22;
+    cout<<arr[2][4];
+    return 0;
+}
