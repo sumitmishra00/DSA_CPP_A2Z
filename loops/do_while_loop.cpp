cde549f888e6ff9e62206b7a547fcecebb7689e0
Sumit Mishra <sumitmishrayt007@gmail.com>
2025-12-06 16:36:49 +0530

    Add files via upload

---
+++ loops/do_while_loop.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i = 2;
+    do 
+    {
+        cout<<"Sumit"<< i << endl;
+
+        i = i + 1 ;
+    }
+
+    while(i<=1);
+    cout<< i << endl;
+    return 0;
+}
