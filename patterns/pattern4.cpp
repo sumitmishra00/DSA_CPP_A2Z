11460920ce92f8093442084cec489d8ba56da609
Sumit Mishra <sumitmishrayt007@gmail.com>
2025-12-09 22:23:04 +0530

    Add files via upload

---
+++ patterns/pattern4.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout<< i << " ";
+        }
+
+        cout<< endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i = 0; i < t; i++)
+    {
+        int n;
+        cin>>n;
+        print(n);
+    }
+}
